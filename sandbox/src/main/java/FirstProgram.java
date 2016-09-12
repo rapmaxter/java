06e3f3657fdf6e691830ac4e119ae258bb296fd3
Max Dumler <msdumler@gmail.com>
2016-09-12 14:30:00 +0200

    CodeStyle

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,7 +1,5 @@
-public class FirstProgram
-{
-public static void main(String[] args)
-{
-System.out.print("Hello, world!");
-}
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+    }
 }
