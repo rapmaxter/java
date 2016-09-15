23477eb2a188e9aa547543c4c9bf91d4bcfd72e2
Max Dumler <msdumler@gmail.com>
2016-09-15 22:01:13 +0200

    update homework1_2

--- sandbox/src/main/java/homework1_2/Point.java
+++ sandbox/src/main/java/homework1_2/Point.java
@@ -4,24 +4,19 @@ package homework1_2;
  */
 public class Point {
 
-  public double x1;
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x;
+  public double y;
 
-  public Point (double x2, double y2) {
 
-    this.x2 = x2;
-    this.y2 = y2;
+  public Point (double x, double y) {
 
+    this.x = x;
+    this.y = y;
   }
 
-  public double distance(double x1, double y1) {
+  public double distance(Point p2) {
 
-    this.x1 = x1;
-    this.y1 = y1;
-
-    double x = (this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1);
+    double x = (this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y);
     return Math.sqrt(x);
   }
 }
