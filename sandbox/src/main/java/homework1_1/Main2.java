ecc2aea6c3898303a021b375630aecaff862dce8
Max Dumler <msdumler@gmail.com>
2016-09-15 21:29:07 +0200

    update homework1_1

--- sandbox/src/main/java/homework1_1/Main2.java
+++ sandbox/src/main/java/homework1_1/Main2.java
@@ -1,4 +1,4 @@
-package homework;
+package homework1_1;
 
 /**
  * Created by Max on 9/12/2016.
@@ -8,19 +8,19 @@ public class Main2 {
   public static void main(String[] args) {
 
     Point p1 = new Point();
-    p1.x1 = 1;
-    p1.y1 = 1;
+    p1.x = 1;
+    p1.y = 1;
 
     Point p2 = new Point();
-    p2.x2 = 3;
-    p2.y2 = 1;
+    p2.x = 3;
+    p2.y = 1;
 
     System.out.println(distance(p1, p2));
   }
 
   public static double distance(Point p1, Point p2) {
 
-    double x = (p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p1.y2 - p2.y1) * (p1.y2 - p2.y1);
+    double x = (p2.x - p1.x) * (p2.x - p1.x) + (p1.y - p2.y) * (p1.y - p2.y);
     return Math.sqrt(x);
   }
 
---
+++ sandbox/src/main/java/homework1_1/Point.java
@@ -0,0 +1,14 @@
+package homework1_1;
+
+/**
+ * Created by Max on 9/12/2016.
+ */
+public class Point {
+
+  public double x; // описываем атрибуты обьекта
+  public double y;
+  ;
+
+
+}
+
