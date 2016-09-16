4fe730b8afcec6d51dcc59c489875f0ba728a945
Max Dumler <msdumler@gmail.com>
2016-09-16 21:29:50 +0200

    Settings for folder Test

---
+++ sandbox/src/test/java/homework1_2/PointTest.java
@@ -0,0 +1,18 @@
+package homework1_2;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Max on 9/16/2016.
+ */
+public class PointTest {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point(1,1);
+
+    Point p2 = new Point(3,1);
+
+    assert p1.distance(p2) == 2;
+  }
+}
