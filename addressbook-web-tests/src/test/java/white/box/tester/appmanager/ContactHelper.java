5cbfa749ca7de9b0f9d978dd3970b61cf86434d2
Max Dumler <msdumler@gmail.com>
2016-09-18 20:46:17 +0200

    homework #5

--- addressbook-web-tests/src/test/java/white/box/tester/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/white/box/tester/appmanager/ContactHelper.java
@@ -17,25 +17,26 @@ public class ContactHelper extends HelperBase {
   }
 
   public void submitCreating() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
+  }
+
+  public void click(By locator) {
+    wd.findElement(locator).click();
   }
 
   public void fillUserField(UserData UserData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(UserData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(UserData.getLastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(UserData.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(UserData.getPhone());
+    type(By.name("lastname"), UserData.getLastname());
+    type(By.name("address"), UserData.getAddress());
+    type(By.name("home"), UserData.getPhone());
+  }
+
+  public void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
   }
 
   public void newUser() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
