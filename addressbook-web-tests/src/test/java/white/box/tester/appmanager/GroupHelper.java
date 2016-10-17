d009a5dde3d7b4ffc4c12569039f3daa3bd7d71f
Max Dumler <msdumler@gmail.com>
2016-10-17 21:46:22 +0200

    Update 6.4

--- addressbook-web-tests/src/test/java/white/box/tester/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/white/box/tester/appmanager/GroupHelper.java
@@ -103,5 +103,7 @@ public class GroupHelper extends HelperBase {
     return new Groups(groupCashe);
   }
 
-
+  public int count() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
 }
--- addressbook-web-tests/src/test/java/white/box/tester/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/white/box/tester/tests/GroupCreationTests.java
@@ -1,22 +1,37 @@
 package white.box.tester.tests;
 
 
-
+import org.omg.CORBA.Object;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import white.box.tester.model.GroupData;
 import white.box.tester.model.Groups;
 
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 
 public class GroupCreationTests extends TestBase {
 
+ @DataProvider
+ public Iterator<Object[]> validGroups() {
+   List<Object[]> list = new ArrayList<Object[]>();
+   list.add(new Object[] {"test1", "header 1", "footer 1"});
+   list.add(new Object[] {"test2", "header 2", "footer 2"});
+   return list.iterator();
+ }
+
+  @Test (dataProvider = "validGroups")
+  public void testsGroupCreation(String name, String header, String footer) {
+
+    }
+
   @Test
-  public void testsGroups() {
+  public void testsBadGroups() {
 
     app.goTo().GroupPage();
     Groups before = app.group().all();
@@ -25,7 +40,7 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
 
-        assertThat(after, equalTo(
+    assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
