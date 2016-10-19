b7e2235575341e6db1e445abae538cef9b933883
Max Dumler <msdumler@gmail.com>
2016-10-19 11:26:07 +0200

    Update 6.6 Contacts

---
+++ addressbook-web-tests/src/test/java/white/box/tester/generators/ContactDataGenerator.java
@@ -0,0 +1,85 @@
+package white.box.tester.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
+import white.box.tester.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Max on 10/19/2016.
+ */
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter (names = "-d", description = "Data format")
+  public String format;
+
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    if (format.equals("csv")){
+      SaveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else {
+      System.out.println("Inkorect format " + format);
+    }
+  }
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xStream = new XStream();
+    xStream.processAnnotations(ContactData.class);
+    String xml = xStream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void SaveAsCsv(List<ContactData> contacts, File file) throws IOException {
+
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s;%s;\n", contact.getFirstname(), contact.getLastname(),
+              contact.getAddress()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstname(String.format("Name %s", i))
+              .withLastname(String.format("Lastname %s", i))
+              .withAddress(String.format("Address %s", i)));
+    }
+    return contacts;
+  }
+
+}
+
