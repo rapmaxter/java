d286de711d772fb65da685b461d9d6beaa181602
Max Dumler <msdumler@gmail.com>
2016-11-02 16:23:16 +0100

    Update 8.4

---
+++ mantis-tests/src/test/java/white/box/mantis/tests/RegistrationTests.java
@@ -0,0 +1,15 @@
+package white.box.mantis.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Max on 11/2/2016.
+ */
+public class RegistrationTests  extends TestBase{
+
+  @Test
+  public void testRegistration(){
+    app.registration().start("user1","user1@localhost.localdomain");
+
+  }
+}
