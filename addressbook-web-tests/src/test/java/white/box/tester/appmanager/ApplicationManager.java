fb1d62c1a322262e195c88291bc9819e5b143082
Max Dumler <msdumler@gmail.com>
2016-11-14 10:53:16 +0100

    Удаленный запуск браузера

--- addressbook-web-tests/src/test/java/white/box/tester/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/white/box/tester/appmanager/ApplicationManager.java
@@ -1,17 +1,21 @@
 package white.box.tester.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -50,13 +54,19 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-      if (browser.equals(BrowserType.FIREFOX)){   // 	 choice browser
-       wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    }else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
-    }
+     if("".equals(properties.getProperty("selenium.server"))) {
+       if (browser.equals(BrowserType.FIREFOX)) {   // 	 choice browser
+         wd = new FirefoxDriver();
+       } else if (browser.equals(BrowserType.CHROME)) {
+         wd = new ChromeDriver();
+       } else if (browser.equals(BrowserType.IE)) {
+         wd = new InternetExplorerDriver();
+       }
+     } else {
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+       capabilities.setBrowserName(browser);
+       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));   // start page(in file local.properties)
     groupHelper = new GroupHelper(wd);
